be3a59c9f6919120497fd370d3cc45fdbe9678d2
p <parimal@beyond8labs.com>
2021-10-07 08:09:05 +1300

    Added pinout for P22B(1)? from KNeutron on discord. https://discord.com/channels/717057001594683422/717057210211106826/895299860004212816

--- ATCwatch/pinout.h
+++ ATCwatch/pinout.h
@@ -1,16 +1,62 @@
-/*
- * Copyright (c) 2020 Aaron Christophel
- *
- * SPDX-License-Identifier: GPL-3.0-or-later
- */
-
 #ifndef _PINOUT_
 #define _PINOUT_
 
-#define P8WATCH//Enable or Disable this line to set for Pinetime or P8 Watch, more pinouts to added here
+#define MODEL_P22
 
-#ifdef P8WATCH
+//P22########################################################################################
+#ifdef MODEL_P22
+//Display
+#define LCD_SCK      2
+#define LCD_SDI      3
+#define LCD_CS       25
+#define LCD_RESET    26
+#define LCD_RS       18
+#define LCD_DET      9
 
+//Flash Chip
+#define SPI_SCK      2
+#define SPI_MOSI     3
+#define SPI_MISO     4
+#define SPI_CE       5
+
+//Touchscreen
+#define TP_SDA       6
+#define TP_SCL       7
+#define TP_RESET     10
+#define TP_INT       28
+
+//Accl Sensor
+#define BMA421_SDA   6
+#define BMA421_SCL   7
+#define BMA421_INT   8
+
+
+//HeartRate Sensor
+#define HRS3300_SDA  6
+#define HRS3300_SCL  7
+#define HRS3300_TEST 30
+
+//Battery
+#define CHARGE_INDICATION 12 
+#define POWER_INDICATION  19
+#define BATTERY_VOLTAGE   31
+#define POWER_CONTROL     24
+
+//InputOutput
+#define STATUS_LED        27
+#define VIBRATOR_OUT      16
+#define PUSH_BUTTON_IN    15  
+#define PUSH_BUTTON_OUT   13 
+
+//Backlight
+#define LCD_BACKLIGHT_LOW  14
+#define LCD_BACKLIGHT_MID  22
+#define LCD_BACKLIGHT_HIGH 23
+
+#else //P22
+
+//P8########################################################################################
+#ifdef MODEL_P8
 //display
 #define LCD_SCK      2
 #define LCD_SDI      3
@@ -28,24 +74,21 @@
 //Touchscreen
 #define TP_SDA       6
 #define TP_SCL       7
-//#define TP_RESET     10 //PinetTime
-#define TP_RESET       13 // P8 Watch
+#define TP_RESET     13
 #define TP_INT       28
 
 //Accl Sensor
-#define BMA421_SDA       6
-#define BMA421_SCL       7
-#define BMA421_INT       8
-//#define SWITCH_X_Y
+#define BMA421_SDA   6
+#define BMA421_SCL   7
+#define BMA421_INT  -1
 
 //HeartRate Sensor
-#define HRS3300_SDA       6
-#define HRS3300_SCL       7
-#define HRS3300_TEST      30
+#define HRS3300_SDA  6
+#define HRS3300_SCL  7
+#define HRS3300_TEST 30
 
 //Battery
-//#define CHARGE_INDICATION 12 // Pinetime
-#define CHARGE_INDICATION -1  // P8 Watch
+#define CHARGE_INDICATION -1
 #define POWER_INDICATION  19
 #define BATTERY_VOLTAGE   31
 #define POWER_CONTROL 24
@@ -53,19 +96,17 @@
 //InputOutput
 #define STATUS_LED        27
 #define VIBRATOR_OUT      16
-//#define PUSH_BUTTON_IN    13 // Pinetime
-//#define PUSH_BUTTON_OUT   15 //PinetTime
-#define PUSH_BUTTON_IN      17   // P8 Watch15//P22
-#define PUSH_BUTTON_OUT   -1   // P8 Watch
+#define PUSH_BUTTON_IN    17
+#define PUSH_BUTTON_OUT   -1
 
 //Backlight
 #define LCD_BACKLIGHT_LOW  14
 #define LCD_BACKLIGHT_MID  22
 #define LCD_BACKLIGHT_HIGH 23
 
-#else //P8WATCH 
+#else //P8
 
-// Pinetime########################################################################################
+//Pinetime########################################################################################
 //display
 #define LCD_SCK      2
 #define LCD_SDI      3
@@ -84,7 +125,6 @@
 #define TP_SDA       6
 #define TP_SCL       7
 #define TP_RESET     10 //PinetTime
-//#define TP_RESET       13 // P8 Watch
 #define TP_INT       28
 
 //Accl Sensor
@@ -99,8 +139,7 @@
 #define HRS3300_TEST      30
 
 //Battery
-#define CHARGE_INDICATION 12 // Pinetime
-//#define CHARGE_INDICATION -1  // P8 Watch
+#define CHARGE_INDICATION 12
 #define POWER_INDICATION  19
 #define BATTERY_VOLTAGE   31
 #define POWER_CONTROL 24
@@ -108,10 +147,8 @@
 //InputOutput
 #define STATUS_LED        27
 #define VIBRATOR_OUT      16
-#define PUSH_BUTTON_IN    13 // Pinetime
-#define PUSH_BUTTON_OUT   15 //PinetTime
-//#define PUSH_BUTTON_IN    17   // P8 Watch
-//#define PUSH_BUTTON_OUT   -1   // P8 Watch
+#define PUSH_BUTTON_IN    13
+#define PUSH_BUTTON_OUT   15
 
 //Backlight
 #define LCD_BACKLIGHT_LOW  14
@@ -119,6 +156,7 @@
 #define LCD_BACKLIGHT_HIGH 23
 
 
-#endif // P8WATCH
+#endif  //P22
+#endif  //P8
 
 #endif // _PINOUT_
